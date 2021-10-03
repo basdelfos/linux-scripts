67af695e0ce996e61b18edf597da4cd8b907a2df
Bas Delfos <b.delfos@gmail.com>
2021-10-03 15:11:13 +0200

    Added CTRL-C trap to update-os

--- update-os.sh
+++ update-os.sh
@@ -5,6 +5,15 @@ echo -e "[*] Version: 0.1"
 
 #
 # 0.1:    Initial version history
+# 0.2:    Added CTRL-C keypress trap to terminate script
+
+# Trap CTRL-C keypress and call terminate()
+trap terminate INT
+
+function terminate() {
+  echo -e "[-] CTRL-C pressed, terminating."
+  exit 1
+}
 
 echo -e "[*] Checking if login with sudo is needed"
 sudo echo -e ""
