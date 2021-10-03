bc8b8c90046d0dda1eceb709221b3de3e3eb577e
Bas Delfos <b.delfos@gmail.com>
2021-10-03 14:06:03 +0200

    Added version history comments in scripts

--- needs-reboot.sh
+++ needs-reboot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#
+# 0.1:    Initial version history
+
 ([ -f /var/run/reboot-required ] && echo '[*] Reboot required' || echo '[+] No reboot required')
--- update-os.sh
+++ update-os.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-echo "[*] Update APT repositories"
+echo -e "[*] Update script Linux OS (debian-based)"
+echo -e "[*] Version: 0.1"
+
+#
+# 0.1:    Initial version history
+
+echo -e "[*] Checking if login with sudo is needed"
+sudo echo -e ""
+
+echo -e "[*] Update APT repositories"
 sudo apt update
 
-echo ""
-echo "[*] Execute distibution update"
+echo -e ""
+echo -e "[*] Execute distibution update"
 sudo apt dist-upgrade -y
 
-echo ""
-echo "[*] Autoremove unused repositories"
+echo -e ""
+echo -e "[*] Autoremove unused repositories"
 sudo apt autoremove -y
 
-echo ""
+echo -e ""
 needs-reboot.sh
