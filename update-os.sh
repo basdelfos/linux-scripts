1834d25eba046db46cc0dfc381358bda0ee487ae
Bas Delfos <b.delfos@gmail.com>
2021-12-10 13:11:47 +0100

    Added option to run update-os without questions

--- update-os.sh
+++ update-os.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo -e "[*] Update script Linux OS (debian-based)"
-echo -e "[*] Version: 0.2"
+echo -e "[*] Version: 0.3"
 
 #
-# 0.1:    Initial version history
-# 0.2:    Added CTRL-C keypress trap to terminate script
+# 0.1:  Initial version history
+# 0.2:	Added CTRL-C keypress trap to terminate script
+# 0.3:	Added commandline option to switch to default anwswer 'Y' to questions
 
 # Trap CTRL-C keypress and call terminate()
 trap terminate INT
@@ -15,6 +16,19 @@ function terminate() {
   exit 1
 }
 
+# Set default values
+YES=""
+
+# Progress commandline arguments
+while getopts y flag; do
+    case "${flag}" in
+      y) 
+        YES="-y"
+	echo -e "[*] Continue without questions enabled, packages will be installed without notice"
+	;;
+    esac
+done
+
 echo -e "[*] Checking if login with sudo is needed"
 sudo echo -e ""
 
@@ -23,11 +37,12 @@ sudo apt update
 
 echo -e ""
 echo -e "[*] Execute distibution update"
-sudo apt dist-upgrade -y
+sudo apt dist-upgrade $YES
 
 echo -e ""
 echo -e "[*] Autoremove unused repositories"
-sudo apt autoremove -y
+sudo apt autoremove $YES
 
 echo -e ""
 needs-reboot.sh
+
