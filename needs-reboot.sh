e59df9143cef7181c4d1dc8b8ed9fff55850735c
Bas Delfos <b.delfos@gmail.com>
2021-12-11 14:51:05 +0100

    Update needs-reboot.sh

--- needs-reboot.sh
+++ needs-reboot.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
-# 0.1:    Initial version history
+# 0.1:  Initial version history
+# 0.2:	Added terminal colors
 
-([ -f /var/run/reboot-required ] && echo '[*] Reboot required' || echo '[+] No reboot required')
+FG_RED="\e[1m\e[31m"
+FG_YLW="\e[1m\e[33m"
+FG_GRE="\e[1m\e[32m"
+CLR="\e[0m"
+
+([ -f /var/run/reboot-required ] && echo -e "$FG_RED[*] Reboot required$CLR" || echo -e "$FG_GRE[+] No reboot required$CLR")
